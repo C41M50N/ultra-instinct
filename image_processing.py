8554a0098364352735c52187d9b0e30430f20a3d
Andrew Jandernoa <andrew.jandernoa@AES.local>
2024-04-24 11:34:04 -0400

    added front camera reading

---
+++ image_processing.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import queue
+import threading
+
+import numpy as np
+from PIL import Image
+from qvl.qcar import QLabsQCar
+
+
+def capture_images(
+    car: QLabsQCar, queue: queue.Queue, kill_signal: threading.Event, camera: QLabsQCar
+):
+    img_num = 1
+    while not kill_signal.is_set():
+        try:
+            front_img_raw = car.get_image(camera)[1]
+            if front_img_raw is not None:
+                queue.put((front_img_raw, img_num))
+            else:
+                print(f"No image captured at index {img_num}")
+            img_num += 1
+        except Exception as e:
+            print(f"Error capturing image at index {img_num}: {e}")
+
+
+def save_images(
+    queue: queue.Queue[np.ndarray], kill_signal: threading.Event, images_dir: Path
+):
+    while not kill_signal.is_set() or not queue.empty():
+        try:
+            # wait for an image to be available
+            front_img_raw, img_num = queue.get(timeout=1)
+            front_img = Image.fromarray(front_img_raw)
+            img_path = images_dir / f"{img_num}.png"
+            front_img.save(img_path)
+            queue.task_done()
+        except queue.empty():
+            continue
+        except Exception as e:
+            print(f"Error saving image {img_num}: {e}")
