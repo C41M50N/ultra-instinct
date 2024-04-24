337e63fda489007ccb32dd1c2d29efd92f6a1baf
Andrew Jandernoa <andrew.jandernoa@AES.local>
2024-04-24 10:34:32 -0400

    added dataset generator

--- setup_environment.py
+++ setup_environment.py
@@ -17,12 +17,12 @@ import pal.resources.rtmodels as rtmodels
 
 
 def setup(
-    initialPosition = [-1.205, -0.83, 0.005],
-    initialOrientation = [0, 0, -44.7],
-    rtModel = rtmodels.QCAR
+    initialPosition=[-1.205, -0.83, 0.005],
+    initialOrientation=[0, 0, -44.7],
+    rtModel=rtmodels.QCAR,
 ):
     # connect to Qlabs
-    os.system('cls')
+    os.system("cls")
     qlabs = QuanserInteractiveLabs()
     print("Connecting to QLabs...")
     try:
@@ -40,9 +40,9 @@ def setup(
     qcar = QLabsQCar(qlabs)
     qcar.spawn_id(
         actorNumber=0,
-        location=[p*10 for p in initialPosition],
+        location=[p * 10 for p in initialPosition],
         rotation=initialOrientation,
-        waitForConfirmation=True
+        waitForConfirmation=True,
     )
 
     # Create a new camera view and attach it to the QCar
@@ -53,59 +53,47 @@ def setup(
     # Spawn stop signs
     stopsign1 = QLabsStopSign(qlabs)
     stopsign1.spawn(
-        location = [24.328, 18.0, 0.0],
-        rotation = [0.0, 0.0, -1.6],
-        waitForConfirmation = False
+        location=[24.328, 18.0, 0.0],
+        rotation=[0.0, 0.0, -1.6],
+        waitForConfirmation=False,
     )
 
     stopsign2 = QLabsStopSign(qlabs)
     stopsign2.spawn(
-        location = [24.328, 2.5, 0.0],
-        rotation = [0.0, 0.0, -1.6],
-        waitForConfirmation = False
+        location=[24.328, 2.5, 0.0],
+        rotation=[0.0, 0.0, -1.6],
+        waitForConfirmation=False,
     )
 
     stopsign3 = QLabsStopSign(qlabs)
     stopsign3.spawn(
-        location = [-10.719, 46.669, 0.185],
-        rotation = [0.0, 0.0, 0.0],
-        waitForConfirmation = False
+        location=[-10.719, 46.669, 0.185],
+        rotation=[0.0, 0.0, 0.0],
+        waitForConfirmation=False,
     )
 
     stopsign4 = QLabsStopSign(qlabs)
     stopsign4.spawn(
-        location = [2.482, 46.673, 0.189],
-        rotation = [0.0, 0.0, 0.0],
-        waitForConfirmation = False
+        location=[2.482, 46.673, 0.189],
+        rotation=[0.0, 0.0, 0.0],
+        waitForConfirmation=False,
     )
 
     # spawn traffic lights
     trafficlight1 = QLabsTrafficLight(qlabs)
-    trafficlight1.spawn(
-        location = [1.108, -12.534, 0.2],
-        rotation = [0.0, 0.0, -1.6]
-    )
+    trafficlight1.spawn(location=[1.108, -12.534, 0.2], rotation=[0.0, 0.0, -1.6])
     trafficlight1.set_state(QLabsTrafficLight.STATE_GREEN)
 
     trafficlight2 = QLabsTrafficLight(qlabs)
-    trafficlight2.spawn(
-        location = [-21.586, 14.403, 0.192],
-        rotation = [0.0, 0.0, math.pi]
-    )
+    trafficlight2.spawn(location=[-21.586, 14.403, 0.192], rotation=[0.0, 0.0, math.pi])
     trafficlight2.set_state(QLabsTrafficLight.STATE_YELLOW)
 
     trafficlight3 = QLabsTrafficLight(qlabs)
-    trafficlight3.spawn(
-        location = [-21.586, 33.136, 0.182],
-        rotation = [0.0, 0.0, math.pi]
-    )
+    trafficlight3.spawn(location=[-21.586, 33.136, 0.182], rotation=[0.0, 0.0, math.pi])
     trafficlight3.set_state(QLabsTrafficLight.STATE_RED)
 
     trafficlight4 = QLabsTrafficLight(qlabs)
-    trafficlight4.spawn(
-        location = [24.271, 32.997, 0.18],
-        rotation = [0.0, 0.0, 0.0]
-    )
+    trafficlight4.spawn(location=[24.271, 32.997, 0.18], rotation=[0.0, 0.0, 0.0])
     trafficlight4.set_state(QLabsTrafficLight.STATE_RED)
 
     # Start spawn model
@@ -117,5 +105,6 @@ def setup(
 def terminate():
     QLabsRealTime().terminate_real_time_model(rtmodels.QCAR_STUDIO)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     setup()
