fa8c88b00f09374b3a66aebe800e5f666e92874c
kris6557 <kris6557@kettering.edu>
2024-06-11 15:15:44 -0400

    got an image from the car

--- car_test.py
+++ car_test.py
@@ -1,19 +1,13 @@
-import multiprocessing
-import os
-from pathlib import Path
-import multiprocessing
-import time
 
-from ultralytics import YOLO
-import cv2
+import os
+
 from qvl.qlabs import QuanserInteractiveLabs
 from qvl.free_camera import QLabsFreeCamera
 from qvl.qcar import QLabsQCar
 from qvl.real_time import QLabsRealTime
-from PIL import Image
 
-from helper_funcs import get_image, run_perception
 import pal.resources.rtmodels as rtmodels
+from pal.products.qcar import IS_PHYSICAL_QCAR, QCarRealSense
 
 
 CAMERA = QLabsQCar.CAMERA_RGB
@@ -21,35 +15,40 @@ CAMERA = QLabsQCar.CAMERA_RGB
 
 def setup_car():
     os.system("cls")
+    
+    if IS_PHYSICAL_QCAR:
+        camera = QCarRealSense("RGB", frameWidthRGB=640, frameHeightRGB=480)
+        img = camera.read_RGB()
+        print(img)
+    else:
+        qlabs = QuanserInteractiveLabs()
+        print("Connecting to QLabs...")
+        try:
+            qlabs.open("localhost")
+            print("Connected to QLabs")
+        except:
+            print("Unable to connect to QLabs")
+            quit()
 
-    qlabs = QuanserInteractiveLabs()
-    print("Connecting to QLabs...")
-    try:
-        qlabs.open("localhost")
-        print("Connected to QLabs")
-    except:
-        print("Unable to connect to QLabs")
-        quit()
+        initialPosition = [-1.205, -0.83, 0.005]
+        initialOrientation = [0, 0, -44.7]
+        # Spawn a QCar at the given initial pose
+        qcar = QLabsQCar(qlabs)
+        qcar.spawn_id(
+            actorNumber=0,
+            location=[p * 10 for p in initialPosition],
+            rotation=initialOrientation,
+            waitForConfirmation=True,
+        )
 
-    initialPosition = [-1.205, -0.83, 0.005]
-    initialOrientation = [0, 0, -44.7]
-    # Spawn a QCar at the given initial pose
-    qcar = QLabsQCar(qlabs)
-    qcar.spawn_id(
-        actorNumber=0,
-        location=[p * 10 for p in initialPosition],
-        rotation=initialOrientation,
-        waitForConfirmation=True,
-    )
+        # Create a new camera view and attach it to the QCar
+        hcamera = QLabsFreeCamera(qlabs)
+        hcamera.spawn()
+        # qcar.possess()
 
-    # Create a new camera view and attach it to the QCar
-    hcamera = QLabsFreeCamera(qlabs)
-    hcamera.spawn()
-    # qcar.possess()
+        QLabsRealTime().start_real_time_model(rtmodels.QCAR)
 
-    QLabsRealTime().start_real_time_model(rtmodels.QCAR)
-
-    return qcar
+        return qcar
 
 
 if __name__ == "__main__":
